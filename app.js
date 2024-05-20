6ea4b442dba0ac39ec77a7ffeb07333fe5304ffd
nOngMolZ <redem.mnd@gmail.com>
2024-05-20 13:23:23 +0700

    Exercise 01 (Part 1)

---
+++ app.js
@@ -0,0 +1,8 @@
+const changeText = () => {
+    let elementh2 = document.getElementById("myHeading");
+    elementh2.innerHTML = "Hello Javascript InnerHTML!";
+}
+const resetText = () => {
+    let elementh2 = document.getElementById("myHeading");
+    elementh2.innerHTML = "Hello World"; 
+}
